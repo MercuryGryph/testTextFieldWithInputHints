7f4dc7e5a002873ee9c723be63c8a7294e8abd29
猫猫鹰 <w84627302@gmail.com>
2025-03-18 10:48:45 +0800

    优化ui设计

--- src/main/kotlin/cn/mercury9/demo/ui/components/OutlinedTextFieldWithHint.kt
+++ src/main/kotlin/cn/mercury9/demo/ui/components/OutlinedTextFieldWithHint.kt
@@ -1,23 +1,28 @@
 package cn.mercury9.demo.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -27,7 +32,6 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,6 +39,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.key.Key
@@ -54,16 +59,15 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
-import cn.mercury9.decoratedText.DecoratedText
-import cn.mercury9.decoratedText.InCardDecoratedText
-import cn.mercury9.decoratedText.UndecoratedText
 import cn.mercury9.demo.data.hintprovider.Hint
 import cn.mercury9.demo.data.hintprovider.HintProvider
 import cn.mercury9.demo.resources.Res
 import cn.mercury9.demo.resources.keyboard_return_24px
 import cn.mercury9.demo.resources.keyboard_tab_24px
 import cn.mercury9.utils.compose.painter
+import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.roundToInt
 
 @ExperimentalLayoutApi
 @Composable
@@ -109,21 +113,13 @@ fun OutlinedTextFieldWithHint(
     var selectedHintIndex by remember { mutableStateOf(0) }
     var firstShowingHintIndex by remember { mutableStateOf(0) }
 
-    val hints = hintProvider.getHintFrom(value, textFieldValue.selection)
+    var hints = hintProvider.getHintFrom(value, textFieldValue.selection)
 
     val lineHeight = textStyle.lineHeight.value
 
-    SideEffect {
-        if (value != textFieldValue.text) {
-            textFieldValue = textFieldValue.copy(text = value)
-            selectedHint = Hint.Empty
-            selectedHintIndex = 0
-            firstShowingHintIndex = 0
-            hintEnabled = true
-        }
-    }
 
-    LaunchedEffect(selectedHintIndex) {
+    fun changeSelectedHintIndexTo(i: Int) {
+        selectedHintIndex = i
         if (selectedHintIndex < 0) selectedHintIndex = 0
         else if (selectedHintIndex > hints.size - 1) selectedHintIndex = hints.size - 1
         else {
@@ -133,6 +129,20 @@ fun OutlinedTextFieldWithHint(
             } else if (firstShowingHintIndex + hintMaxShowingNumber - 1 < selectedHintIndex) {
                 firstShowingHintIndex = selectedHintIndex - hintMaxShowingNumber + 1
             }
+        }
+    }
+
+    fun changeSelectedHint(delta: Int) {
+        changeSelectedHintIndexTo(selectedHintIndex + delta)
+    }
+
+    SideEffect {
+        if (value != textFieldValue.text) {
+            textFieldValue = textFieldValue.copy(text = value)
+            selectedHint = Hint.Empty
+            changeSelectedHintIndexTo(0)
+            firstShowingHintIndex = 0
+            hintEnabled = true
         }
     }
 
@@ -189,10 +199,36 @@ fun OutlinedTextFieldWithHint(
                     ) {
                         return false
                     } else if (event.isShiftPressed) {
-                        selectedHintIndex -= 1
+                        changeSelectedHint(-1)
                     } else {
-                        selectedHintIndex += 1
+                        changeSelectedHint(1)
                     }
+                    return true
+                }
+
+                Key.DirectionUp -> {
+                    if (
+                        event.isAltPressed ||
+                        event.isCtrlPressed ||
+                        event.isMetaPressed ||
+                        event.isShiftPressed
+                    ) {
+                        return false
+                    }
+                    changeSelectedHint(-1)
+                    return true
+                }
+
+                Key.DirectionDown -> {
+                    if (
+                        event.isAltPressed ||
+                        event.isCtrlPressed ||
+                        event.isMetaPressed ||
+                        event.isShiftPressed
+                    ) {
+                        return false
+                    }
+                    changeSelectedHint(1)
                     return true
                 }
             }
@@ -211,10 +247,11 @@ fun OutlinedTextFieldWithHint(
             onValueChange = {
                 textFieldValue = it
                 hintEnabled = true
-                selectedHintIndex = run {
+                hints = hintProvider.getHintFrom(it.text, it.selection)
+                changeSelectedHintIndexTo(run {
                     val i = hints.indexOf(selectedHint)
                     if (i == -1) 0 else i
-                }
+                })
                 onValueChange(it.text)
             },
             //region unchanged OutlinedTextField params
@@ -251,11 +288,22 @@ fun OutlinedTextFieldWithHint(
                     modifier = Modifier
                         .padding(8.dp)
                         .width(250.dp)
+                        .scrollable(
+                            state = rememberScrollableState { delta ->
+                                changeSelectedHint(-(delta / 15).roundToInt())
+                                delta
+                            },
+                            orientation = Orientation.Vertical,
+                        )
                 ) {
-                    Column {
+                    Row(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .height(IntrinsicSize.Min)
+                    ) {
                         Column(
                             modifier = Modifier
-                                .padding(4.dp)
+                                .weight(1f)
                         ) {
                             if (firstShowingHintIndex > 0) {
                                 HintMoreToShow()
@@ -263,6 +311,10 @@ fun OutlinedTextFieldWithHint(
 
                             for (index in 0 until min(hintMaxShowingNumber, hints.size)) {
                                 val selfIndex = firstShowingHintIndex + index
+                                if (selfIndex > hints.size - 1) {
+                                    changeSelectedHintIndexTo(0)
+                                    break
+                                }
                                 val selected = selfIndex == selectedHintIndex
                                 HintItem(
                                     hint = hints[selfIndex].hint,
@@ -271,7 +323,7 @@ fun OutlinedTextFieldWithHint(
                                     if (selected) {
                                         useHint()
                                     } else {
-                                        selectedHintIndex = selfIndex
+                                        changeSelectedHintIndexTo(selfIndex)
                                     }
                                 }
                             }
@@ -280,13 +332,29 @@ fun OutlinedTextFieldWithHint(
                                 HintMoreToShow()
                             }
                         }
-                        HorizontalDivider()
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(MaterialTheme.colorScheme.surface)
-                        ) {
-                            HintOperations()
+                        if (hints.size > hintMaxShowingNumber) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .padding(2.dp)
+                                    .width(12.dp)
+                                    .border(1.dp, MaterialTheme.colorScheme.outlineVariant, CircleShape)
+                            ) {
+                                Spacer(Modifier
+                                    .weight(max(firstShowingHintIndex.toFloat(), .01f))
+                                )
+                                Box(Modifier
+                                    .weight(hintMaxShowingNumber + 1f)
+                                    .fillMaxWidth()
+                                    .padding(2.dp)
+                                    .clip(CircleShape)
+                                    .background(MaterialTheme.colorScheme.outlineVariant)
+                                )
+                                Spacer(Modifier
+                                    .weight(max(hints.size - firstShowingHintIndex.toFloat() - hintMaxShowingNumber, .01f))
+                                )
+                            }
                         }
                     }
                 }
@@ -306,7 +374,6 @@ private fun HintItem(
         modifier = Modifier
             .padding(1.dp)
             .height(IntrinsicSize.Min)
-            .clickable { onClick() },
     ) {
         Box(
             Modifier
@@ -318,6 +385,7 @@ private fun HintItem(
                         Color.Transparent
                     }
                 )
+                .clickable { onClick() },
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -355,32 +423,4 @@ private fun HintMoreToShow() {
         modifier = Modifier
             .padding(horizontal = 4.dp)
     )
-}
-
-private val hintOperations: List<DecoratedText> = listOf(
-    InCardDecoratedText("Tab")
-) + UndecoratedText.splitWhitespace(" to select UP, ") +
-    InCardDecoratedText("Shift") +
-    UndecoratedText.splitWhitespace(" + ") +
-    InCardDecoratedText("Tab") +
-    UndecoratedText.splitWhitespace(" to select Down, ") +
-    InCardDecoratedText("Enter") +
-    UndecoratedText.splitWhitespace(" to Input.")
-
-@ExperimentalLayoutApi
-@Composable
-private fun HintOperations(
-    style: TextStyle = MaterialTheme.typography.labelSmall,
-    color: Color = MaterialTheme.colorScheme.outline,
-    modifier: Modifier = Modifier.padding(8.dp)
-) {
-    FlowRow(
-        modifier = modifier,
-    ) {
-        DecoratedText.composeAll(
-            hintOperations,
-            style = style,
-            color = color,
-        )
-    }
 }
